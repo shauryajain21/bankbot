fac0c7c443a48ceedca01b0e56d7c9941d8a39d6
Shaurya Jain <72522908+shauryajain21@users.noreply.github.com>
2024-12-09 22:38:16 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 require("dotenv").config();
 
 // Middleware
